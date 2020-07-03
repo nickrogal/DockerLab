fddfe1a9ca7430b534636c2f97b1878a00239698
Nicholas Rogalsky <nicholas@MacBook-Pro.localdomain>
2020-07-03 17:51:36 -0400

    structured better

--- dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ubuntu:latest
-COPY . /test
-RUN apt-get update -y
-RUN sh test/ldaptest.sh
-#CMD ["sh","test/ldapTest.sh"]
--- ldapTest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt install openssh-server -y
-ufw allow ssh
-apt install slapd ldap-utils -y
-dpkg-reconfigure slapd
-apt install phpldapadmin -y
