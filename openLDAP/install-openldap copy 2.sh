2c699e897a623d2a59d9b2a92e5df865605df64c
Nicholas Rogalsky <nicholas@MacBook-Pro.localdomain>
2020-07-05 16:32:30 -0400

    playing around

--- openLDAP/install-openldap copy 2.sh
+++ openLDAP/install-openldap copy 2.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#apt-get install openssh-server -y
+
+#bellow taken from https://unix.stackexchange.com/questions/362547/automating-slapd-install
+# install maven 
+
+#apt-get install -y maven 
+#apt-get -y install php php-cgi libapache2-mod-php php-common php-pear php-mbstring 
+
+#a2enconf php7.4-cgi 
+
+#service apache2 restart 
+
 
 # Pre-seed the slapd passwords 
 
@@ -15,4 +27,4 @@ apt-get install -y slapd ldap-utils phpldapadmin
   #&& echo 'slapd slapd/domain string it610.local' | debconf-set-selections \
   #&& echo -e "slapd slapd/password2 password test" |debconf-set-selections
 
-#dpkg-reconfigure -f noninteractive slapd
+dpkg-reconfigure -f noninteractive slapd
